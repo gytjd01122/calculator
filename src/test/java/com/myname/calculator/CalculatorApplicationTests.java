f97cf8117c5ad93cfc32d5d173b8df9c994f8812
KIM HYO SEONG <5171699@stu.kmu.ac.kr>
2019-04-16 15:27:55 +0900

    add Junit test code

--- src/test/java/com/myname/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/myname/calculator/CalculatorApplicationTests.java
@@ -1,6 +1,7 @@
 package com.myname.calculator;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,9 +9,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorApplicationTests {
-
+	private Calculator calculator = new Calculator();
 	@Test
 	public void contextLoads() {
 	}
+    
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+
 
 }
