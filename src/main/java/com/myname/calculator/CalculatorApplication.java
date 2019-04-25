ff3fe2004646efbfc44e9fd3d04f4af6048e7987
KIM HYO SEONG <5171699@stu.kmu.ac.kr>
2019-04-25 15:50:46 +0900

    Added: add dockerfile , comment following checkstyle rule

--- src/main/java/com/myname/calculator/CalculatorApplication.java
+++ src/main/java/com/myname/calculator/CalculatorApplication.java
@@ -2,16 +2,12 @@ package com.myname.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/**
- * This is Test Sample.
- */
+
+/** This is sample JAVADOC comments
+    Just a Sample
+*/
 @SpringBootApplication
 public class CalculatorApplication {
-	/**
-	 * 
-	 * @param args args.
-	 */
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
--- src/test/java/com/myname/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/myname/calculator/CalculatorApplicationTests.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+/** This is sample JAVADOC comments
+    Just a Sample
+*/
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorApplicationTests {
